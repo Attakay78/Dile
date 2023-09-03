5e80ebedf012bd357eb7cd4c121efb7e6618bcaf
Attakay78 <richardquaicoe78@gmail.com>
2023-09-03 20:11:30 +0000

    Added FieldTypeError and MaxLengthExceeded Exceptions.

--- exceptions.py
+++ exceptions.py
@@ -2,7 +2,10 @@ class ColumnDoesNotExist(Exception):
     def __init__(self, message):
         super().__init__(self, message)
 
-
 class FieldTypeError(Exception):
+    def __init__(self, message):
+        super().__init__(self, message)
+
+class MaxLengthExceeded(Exception):
     def __init__(self, message):
         super().__init__(self, message)
