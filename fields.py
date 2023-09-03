b495ae7bc2b93c75b022c0745a92f0fd49b72dd1
Attakay78 <richardquaicoe78@gmail.com>
2023-09-03 20:12:49 +0000

    Updated Field to accomodate options like max_length, default, name ...

--- fields.py
+++ fields.py
@@ -1,21 +1,47 @@
-from exceptions import FieldTypeError
+from exceptions import FieldTypeError, MaxLengthExceeded
 
 
 # Field descriptor for setting fields for columns
 class Field:
+    def __init__(
+        self,
+        name=None,
+        max_length=None,
+        null=False, #Yet to
+        unique=False, #Yet to
+        default=None,
+        editable=True, #Yet to
+        serialize=True, #Yet to
+        choices=None, #Yet to
+    ):
+        self.field_name = name
+        self.max_length = max_length
+        self.is_nullable = null
+        self._unique = unique
+        self.default = default
+        self.is_editable = editable
+        self.is_serializable = serialize
+        self.choices = choices
+    
+    def check_max_length(self, value):
+        if self.max_length and len(value) > self.max_length:
+            raise MaxLengthExceeded(f"Maximum length exceeded, expected max of {self.max_length}, but got {len(value)}")
+
     def __set_name__(self, owner, name):
-        self.name = name
+        if self.field_name:
+            self.name = self.field_name
+        else:
+            self.name = name
     
     def __get__(self, instance, owner):
-        return instance.__dict__["fields"].get(self.name).field_value
+        return instance.__dict__.get(self.name)
 
     def __set__(self, instance, value):
-        instance.__dict__["fields"][self.name].field_value = value
+        self.check_max_length(value)
+        instance.__dict__[self.name]= value
 
 
 class CharField(Field):
-    def __init__(self, max_length=10, **options):
-        self.max_length = max_length
 
     def __set__(self, instance, value):
         if type(value) != str:
@@ -30,7 +56,7 @@ class TextField(Field):
 
 class IntField(Field):
     def __set__(self, instance, value):
-        if type(value) != int:
+        if type(value) != int and value != None:
             msg = f"{value} expected type is int but {type(value)} given."
             raise FieldTypeError(msg)
         super().__set__(instance, value)
