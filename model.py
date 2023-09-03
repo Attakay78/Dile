db45cea920e06b923d5a449c1e8c1e823753ee5d
Attakay78 <richardquaicoe78@gmail.com>
2023-09-03 20:14:00 +0000

    Added logic to set default fields values.

--- model.py
+++ model.py
@@ -18,20 +18,32 @@ class ModelBase(type):
 class Model(metaclass=ModelBase):
     def __init__(self, **kwargs):
         # get dict value of subclasses
-        subclass_dict = self.__class__.__dict__
+        subclass_dict = self.__class__.__dict__.copy()
         instance_dict = self.__dict__
-        instance_dict["fields"] = {}
         
         # check the base classes of all the props in subclass_dict and add to the instance dict 
         # all props with base Field
         for prop, value in subclass_dict.items():
             base_classes = value.__class__.__bases__
             if base_classes and (Field in base_classes):
-                instance_dict["fields"][prop] = Model_Subclass(field_type=value)
+                instance_dict[prop] = value
+
+                field_name = instance_dict[prop].field_name
+                if field_name:
+                    instance_dict[field_name] = instance_dict.pop(prop)
+                    setattr(self.__class__, field_name, value)
         
+        # Set key, value from model fields provided. Raise error if column does not exist
         for key, value in kwargs.items():
-            if key in instance_dict["fields"]:
-                instance_dict["fields"][key].field_type.__set__(self, value)
-                pass
+            if key in instance_dict:
+                instance_dict[key].__set__(self, value)
             else:
                 raise ColumnDoesNotExist(f"{key} not a field in {self.__class__.__name__}")
+        
+        # Set Default fields values
+        if len(kwargs) < len(instance_dict):
+            for field, value in instance_dict.items():
+                if field not in kwargs.keys() and value.default != None:
+                    instance_dict[field].__set__(self, value.default)
+                elif field not in kwargs.keys():
+                    instance_dict[field].__set__(self, None)
