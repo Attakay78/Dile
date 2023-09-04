d8a5dbb5b5d712b696b55471cc160d8695399cfe
Attakay78 <richardquaicoe78@gmail.com>
2023-09-04 22:15:34 +0000

    Implemented check for fields nullable parameter.

--- fields.py
+++ fields.py
@@ -1,51 +1,58 @@
-from exceptions import FieldTypeError, MaxLengthExceeded
+from exceptions import FieldTypeError, MaxLengthExceeded, FieldCantBeNull
+from typing import Any
 
 
 # Field descriptor for setting fields for columns
 class Field:
     def __init__(
         self,
-        name=None,
-        max_length=None,
-        null=False, #Yet to
-        unique=False, #Yet to
-        default=None,
-        editable=True, #Yet to
-        serialize=True, #Yet to
-        choices=None, #Yet to
+        name: str = None,
+        max_length: int = None,
+        nullable: bool = True,
+        unique: bool = False,  # Yet to
+        default: Any = None,
+        editable: bool = True,  # Yet to
+        serialize: bool = True,  # Yet to
+        choices: Any = None,  # Yet to
     ):
         self.field_name = name
         self.max_length = max_length
-        self.is_nullable = null
+        self.is_nullable = nullable
         self._unique = unique
         self.default = default
         self.is_editable = editable
         self.is_serializable = serialize
         self.choices = choices
-    
-    def check_max_length(self, value):
-        if self.max_length and len(value) > self.max_length:
-            raise MaxLengthExceeded(f"Maximum length exceeded, expected max of {self.max_length}, but got {len(value)}")
 
-    def __set_name__(self, owner, name):
+    def __check_max_length(self, value: Any):
+        if self.max_length and len(value) > self.max_length:
+            msg: str = f"Maximum length exceeded, expected max of {self.max_length}, but got {len(value)}"
+            raise MaxLengthExceeded(msg)
+
+    def __check_is_nullable(self, value: Any):
+        if self.is_nullable == False and value == None:
+            msg: str = f"Field {self.name} can't be null"
+            raise FieldCantBeNull(msg)
+
+    def __set_name__(self, owner, name: str):
         if self.field_name:
             self.name = self.field_name
         else:
             self.name = name
-    
-    def __get__(self, instance, owner):
+
+    def __get__(self, instance, owner) -> Any:
         return instance.__dict__.get(self.name)
 
-    def __set__(self, instance, value):
-        self.check_max_length(value)
-        instance.__dict__[self.name]= value
+    def __set__(self, instance, value: Any):
+        self.__check_max_length(value)
+        self.__check_is_nullable(value)
+        instance.__dict__[self.name] = value
 
 
 class CharField(Field):
-
-    def __set__(self, instance, value):
+    def __set__(self, instance, value: str):
         if type(value) != str and value != None:
-            msg = f"{value} expected type is str but {type(value)} given."
+            msg: str = f"{value} expected type is str but {type(value)} given."
             raise FieldTypeError(msg)
         super().__set__(instance, value)
 
@@ -55,8 +62,8 @@ class TextField(Field):
 
 
 class IntField(Field):
-    def __set__(self, instance, value):
+    def __set__(self, instance, value: int):
         if type(value) != int and value != None:
-            msg = f"{value} expected type is int but {type(value)} given."
+            msg: str = f"{value} expected type is int but {type(value)} given."
             raise FieldTypeError(msg)
         super().__set__(instance, value)
